74a06241d4a6fdc4cb90863e34e99fb2ad3434ed
Siddharth Bhat <dbgt4531@gmail.com>
2024-11-14 20:49:37 +0530

    using event listener

--- module-2-essentials/essential-project/src/app/user/user.component.ts
+++ module-2-essentials/essential-project/src/app/user/user.component.ts
@@ -11,7 +11,12 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 })
 export class UserComponent {
   selectedUser = DUMMY_USERS[randomIndex];
+  
   get imagePath() {
     return 'assets/users/' + this.selectedUser.avatar;
+  }
+  
+  onSelectUser() {
+    console.log('Clicked!');
   }
 }
