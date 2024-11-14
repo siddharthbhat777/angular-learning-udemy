f060a562ba0a524a1190a9e54a19e700941f11dd
Siddharth Bhat <dbgt4531@gmail.com>
2024-11-14 20:45:37 +0530

    dynamic data bunding and using getter

--- module-2-essentials/essential-project/src/app/user/user.component.ts
+++ module-2-essentials/essential-project/src/app/user/user.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { DUMMY_USERS } from '../dummy-users';
+
+const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 
 @Component({
   selector: 'app-user',
@@ -6,4 +9,9 @@ import { Component } from '@angular/core';
   templateUrl: './user.component.html',
   styleUrl: './user.component.css'
 })
-export class UserComponent {}
+export class UserComponent {
+  selectedUser = DUMMY_USERS[randomIndex];
+  get imagePath() {
+    return 'assets/users/' + this.selectedUser.avatar;
+  }
+}
