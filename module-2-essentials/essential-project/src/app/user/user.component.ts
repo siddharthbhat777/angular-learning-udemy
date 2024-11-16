232b87fcfda983af07635453a7b5e6f0f1b131f0
Siddharth Bhat <dbgt4531@gmail.com>
2024-11-16 08:57:14 +0530

    using output signals

--- module-2-essentials/essential-project/src/app/user/user.component.ts
+++ module-2-essentials/essential-project/src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, output, Output } from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -10,7 +10,8 @@ export class UserComponent {
   @Input({ required: true }) id!: string;
   @Input({ required: true }) avatar!: string; // @Input decorator basically used to accept props passed by parent component
   @Input({ required: true }) name!: string;
-  @Output() select = new EventEmitter(); // send data to parent component
+  // @Output() select = new EventEmitter(); // send data to parent component
+  select = output<string>()
 
   get imagePath() {
     return 'assets/users/' + this.avatar;
