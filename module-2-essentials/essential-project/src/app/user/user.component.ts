fe8508b06c95252c0562de1d692e41d9bdf2cceb
Siddharth Bhat <dbgt4531@gmail.com>
2024-11-16 11:06:12 +0530

    display username

--- module-2-essentials/essential-project/src/app/user/user.component.ts
+++ module-2-essentials/essential-project/src/app/user/user.component.ts
@@ -10,7 +10,7 @@ export class UserComponent {
   @Input({ required: true }) id!: string;
   @Input({ required: true }) avatar!: string; // @Input decorator basically used to accept props passed by parent component
   @Input({ required: true }) name!: string;
-  @Output() select = new EventEmitter(); // created output variable
+  @Output() select = new EventEmitter<string>(); // created output variable
 
   get imagePath() {
     return 'assets/users/' + this.avatar;
