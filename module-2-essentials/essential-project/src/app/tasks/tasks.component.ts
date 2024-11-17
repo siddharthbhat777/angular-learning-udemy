e47fb20608d795ac43bce835eb97def57a23fb31
Siddharth Bhat <dbgt4531@gmail.com>
2024-11-18 05:08:16 +0530

    added complete task feature

--- module-2-essentials/essential-project/src/app/tasks/tasks.component.ts
+++ module-2-essentials/essential-project/src/app/tasks/tasks.component.ts
@@ -38,4 +38,8 @@ export class TasksComponent {
   get selectedUserTasks() {
     return this.tasks.filter((task) => task.userId === this.userId);
   }
+
+  onCompleteTask(id: string) {
+    this.tasks = this.tasks.filter((task) => task.id !== id);
+  }
 }
