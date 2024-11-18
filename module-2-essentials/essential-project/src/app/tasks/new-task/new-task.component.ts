5e19abdc8fa730119a7ed29aa0f60d554bbfb058
Siddharth Bhat <dbgt4531@gmail.com>
2024-11-18 23:07:43 +0530

    made data as service and added dependency injection everywhere

--- module-2-essentials/essential-project/src/app/tasks/new-task/new-task.component.ts
+++ module-2-essentials/essential-project/src/app/tasks/new-task/new-task.component.ts
@@ -1,6 +1,6 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, inject, Input, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { type NewTaskData } from '../task/task.model';
+import { TasksService } from '../tasks.service';
 
 @Component({
   selector: 'app-new-task',
@@ -10,21 +10,29 @@ import { type NewTaskData } from '../task/task.model';
   styleUrl: './new-task.component.css'
 })
 export class NewTaskComponent {
-  @Output() cancel = new EventEmitter<void>(); // voied because not passing any data
-  @Output() add = new EventEmitter<NewTaskData>();
+  @Input({ required: true }) userId!: string;
+  @Output() close = new EventEmitter<void>(); // voied because not passing any data
   enteredTitle = '';
   enteredSummary = '';
   enteredDate = '';
 
+  // constructor(private tasksService: TasksService) {} // This also works
+
+  // Other way
+  private tasksService = inject(TasksService);
+
   onCancel() {
-    this.cancel.emit();
+    this.close.emit();
   }
 
   onSubmit() {
-    this.add.emit({
+    this.tasksService.addTask({
       title: this.enteredTitle,
       summary: this.enteredSummary,
       date: this.enteredDate
-    });
+    },
+      this.userId
+    );
+    this.close.emit();
   }
 }
