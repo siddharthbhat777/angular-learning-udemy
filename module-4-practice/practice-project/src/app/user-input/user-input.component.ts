bcaa8b400efdcb07f629d71830d9c7fad201581d
Siddharth Bhat <dbgt4531@gmail.com>
2024-11-20 08:09:30 +0530

    printing result of investments

--- module-4-practice/practice-project/src/app/user-input/user-input.component.ts
+++ module-4-practice/practice-project/src/app/user-input/user-input.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { InvestmentInput } from '../investment.model';
 
 @Component({
   selector: 'app-user-input',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './user-input.component.html',
   styleUrl: './user-input.component.css'
 })
 export class UserInputComponent {
+  calculate = output<InvestmentInput>();
+  enteredInitialInvestment = '0'; // string because input will always emit string values, though later it gets converted
+  enteredAnnualInvestment = '0';
+  enteredExpectedReturn = '0';
+  enteredDuration = '10';
 
+  onSubmit() {
+    this.calculate.emit({
+      initialInvestment: +this.enteredInitialInvestment,
+      annualInvestment: +this.enteredAnnualInvestment,
+      expectedReturn: +this.enteredExpectedReturn,
+      duration: +this.enteredDuration
+    })
+  }
 }
