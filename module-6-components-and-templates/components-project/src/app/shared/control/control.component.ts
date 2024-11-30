01b045cf29c79a5809ee983f81f79556d5349bcb
Siddharth Bhat <dbgt4531@gmail.com>
2024-11-30 22:24:31 +0530

    adding class to host element

--- module-6-components-and-templates/components-project/src/app/shared/control/control.component.ts
+++ module-6-components-and-templates/components-project/src/app/shared/control/control.component.ts
@@ -6,7 +6,10 @@ import { Component, input, ViewEncapsulation } from '@angular/core';
   imports: [],
   templateUrl: './control.component.html',
   styleUrl: './control.component.css',
-  encapsulation: ViewEncapsulation.None // for adding input, textarea styles which will be loaded
+  encapsulation: ViewEncapsulation.None, // for adding input, textarea styles which will be loaded
+  host: {
+    class: 'control'
+  }
 })
 export class ControlComponent {
   label = input.required<string>();
