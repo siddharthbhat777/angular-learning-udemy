a89fa91b3b91579e17a88b59970dc3808b222abd
Siddharth Bhat <dbgt4531@gmail.com>
2024-11-30 21:13:49 +0530

    made styling component specific and made few changes in ts, html and also css file
    1. control.component.ts
    added
    encapsulation: ViewEncapsulation.None
    
    2. button.component.css
    added :host to target host element
    
    3. added classes and ids in several html files to target their css

--- module-6-components-and-templates/components-project/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ module-6-components-and-templates/components-project/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { ControlComponent } from "../../../shared/control/control.component";
+import { ButtonComponent } from '../../../shared/button/button.component';
 
 @Component({
   selector: 'app-new-ticket',
   standalone: true,
-  imports: [ControlComponent],
+  imports: [ControlComponent, ButtonComponent],
   templateUrl: './new-ticket.component.html',
   styleUrl: './new-ticket.component.css'
 })
