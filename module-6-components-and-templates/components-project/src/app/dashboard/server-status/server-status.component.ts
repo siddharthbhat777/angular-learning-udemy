de4dfbbbeaf938213b98ff01a43e5385efa8a0ba
Siddharth Bhat <dbgt4531@gmail.com>
2024-12-03 16:52:01 +0530

    ngOnDestroy latest alternative

--- module-6-components-and-templates/components-project/src/app/dashboard/server-status/server-status.component.ts
+++ module-6-components-and-templates/components-project/src/app/dashboard/server-status/server-status.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, DestroyRef, inject, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-server-status',
@@ -7,13 +7,14 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
   templateUrl: './server-status.component.html',
   styleUrl: './server-status.component.css'
 })
-export class ServerStatusComponent implements OnInit, OnDestroy { // no need to implement, just do to avoid ngOnInit typo
+export class ServerStatusComponent implements OnInit { // no need to implement, just do to avoid ngOnInit typo
   currentStatus: 'online' | 'offline' | 'unknown' = 'online'; // setting values as data types
-  private interval?: ReturnType<typeof setInterval>;
+  // private interval?: ReturnType<typeof setInterval>;
+  private destroyRef = inject(DestroyRef);
 
   ngOnInit() {
     console.log('ON INIT');
-    setInterval(() => {
+    const interval = setInterval(() => {
       const rnd = Math.random();
       if (rnd < 0.5) {
         this.currentStatus = 'online';
@@ -23,13 +24,17 @@ export class ServerStatusComponent implements OnInit, OnDestroy { // no need to 
         this.currentStatus = 'unknown';
       }
     }, 5000);
+    
+    this.destroyRef.onDestroy(() => {
+      clearInterval(interval);
+    });
   }
 
   ngAfterViewInit() {
     console.log('AFTER VIEW INIT');
   }
 
-  ngOnDestroy() {
+  /* ngOnDestroy() {
     clearTimeout(this.interval);
-  }
+  } */
 }
