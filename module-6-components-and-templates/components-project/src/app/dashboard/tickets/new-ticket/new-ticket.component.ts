5fc2e171513bc4c3446f327428bf0ba045303738
Siddharth Bhat <dbgt4531@gmail.com>
2024-12-03 17:18:24 +0530

    getting form inout values

--- module-6-components-and-templates/components-project/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ module-6-components-and-templates/components-project/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -11,7 +11,7 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './new-ticket.component.css'
 })
 export class NewTicketComponent {
-  onSubmit(titleElement: HTMLInputElement) {
-    console.log(titleElement.value);
+  onSubmit(title: string, text: string) {
+    console.log(title, text);
   }
 }
