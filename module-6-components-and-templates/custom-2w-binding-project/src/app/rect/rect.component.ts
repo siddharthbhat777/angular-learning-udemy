3ee802f17df6dae3d0432eeac39f9f9de62e8c4c
Siddharth Bhat <dbgt4531@gmail.com>
2024-12-03 23:48:08 +0530

    using signals for custom two way binding

--- module-6-components-and-templates/custom-2w-binding-project/src/app/rect/rect.component.ts
+++ module-6-components-and-templates/custom-2w-binding-project/src/app/rect/rect.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, input, model, Output } from '@angular/core';
 
 @Component({
   selector: 'app-rect',
@@ -9,12 +9,19 @@ import { Component, EventEmitter, Input, input, Output } from '@angular/core';
 })
 export class RectComponent {
   // Implemented custom two-way binding
-  @Input({ required: true }) size!: { width: string; height: string; };
-  @Output() sizeChange = new EventEmitter<{ width: string; height: string; }>(); 
+  /* @Input({ required: true }) size!: { width: string; height: string; };
+  @Output() sizeChange = new EventEmitter<{ width: string; height: string; }>();  */
   // naming is critical here for output for two-way binding it should be "<input_variable_name>Change"
 
+  // new way
+  size = model.required<{ width: string; height: string; }>();
+
   onReset() {
-    this.sizeChange.emit({
+    /* this.sizeChange.emit({
+      width: '200',
+      height: '100'
+    }); */
+    this.size.set({
       width: '200',
       height: '100'
     });
