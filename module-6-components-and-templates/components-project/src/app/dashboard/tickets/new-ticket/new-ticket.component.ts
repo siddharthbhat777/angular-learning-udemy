f8fca8a2819d9c62af8918e4e6d87a57d3c8383a
Siddharth Bhat <dbgt4531@gmail.com>
2024-12-03 18:42:25 +0530

    using signal alternative

--- module-6-components-and-templates/components-project/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ module-6-components-and-templates/components-project/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component, ElementRef, viewChild } from '@angular/core';
 import { ControlComponent } from "../../../shared/control/control.component";
 import { ButtonComponent } from '../../../shared/button/button.component';
 import { FormsModule } from '@angular/forms';
@@ -11,11 +11,12 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './new-ticket.component.css'
 })
 export class NewTicketComponent {
-  @ViewChild('formTemplate') form?: ElementRef<HTMLFormElement>;
+  // @ViewChild('formTemplate') form?: ElementRef<HTMLFormElement>; // old way
+  private form = viewChild.required<ElementRef<HTMLFormElement>>('formTemplate');
 
   onSubmit(title: string, text: string) {
     console.log(title, text);
-    this.form?.nativeElement.reset();
+    this.form()?.nativeElement.reset();
     // this.form -> we are accessing ElementRef
     // this.form.nativeElement -> we are accessing actual form element
   }
