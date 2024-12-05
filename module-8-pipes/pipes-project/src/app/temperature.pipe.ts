11935caa72a8df9cb03a9e8765f1641f33d407c9
Siddharth Bhat <dbgt4531@gmail.com>
2024-12-05 20:03:08 +0530

    using custom pipes to modify data

--- module-8-pipes/pipes-project/src/app/temperature.pipe.ts
+++ module-8-pipes/pipes-project/src/app/temperature.pipe.ts
@@ -5,7 +5,11 @@ import { Pipe, PipeTransform } from "@angular/core";
     standalone: true
 })
 export class TemperaturePipe implements PipeTransform {
-    transform(value: string | number, inputType: 'cel' | 'fah', outputType?: 'cel' | 'fah') {
+    transform(value: string | number | null, inputType: 'cel' | 'fah', outputType?: 'cel' | 'fah') {
+        if (!value) {
+            return value;
+        }
+
         let val: number;
         if (typeof value === 'string') {
             val = parseFloat(value);
@@ -30,6 +34,6 @@ export class TemperaturePipe implements PipeTransform {
             symbol = outputType === 'cel' ? '°C' : '°F';
         }
 
-        return `${outputTemp} ${symbol}`;
+        return `${outputTemp.toFixed(2)} ${symbol}`;
     }
 }
