d49825a86956eaedabef80ec411ec5ef765b5585
Siddharth Bhat <dbgt4531@gmail.com>
2024-12-05 20:49:10 +0530

    created addTask function in service

---
+++ module-9-services-and-dependency-injection/services-project/src/app/tasks/tasks.service.ts
@@ -0,0 +1,18 @@
+import { Injectable, signal } from "@angular/core";
+import { Task } from "./task.model";
+
+@Injectable({
+    providedIn: "root"
+})
+export class TaskService {
+    tasks = signal<Task[]>([]);
+
+    addTasks(taskData: { title: string; description: string }) {
+        const newTask: Task = {
+            ...taskData,
+            id: Math.random().toString(),
+            status: 'OPEN'
+        };
+        this.tasks.update((oldTasks) => [...oldTasks, newTask])
+    }
+}
