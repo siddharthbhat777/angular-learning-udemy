3de7d46a669dd7dabf3da657d71a03de08b73738
Siddharth Bhat <dbgt4531@gmail.com>
2024-12-05 21:03:11 +0530

    making dependency injection

--- module-9-services-and-dependency-injection/services-project/src/app/tasks/tasks.service.ts
+++ module-9-services-and-dependency-injection/services-project/src/app/tasks/tasks.service.ts
@@ -4,10 +4,10 @@ import { Task } from "./task.model";
 @Injectable({
     providedIn: "root"
 })
-export class TaskService {
+export class TasksService {
     tasks = signal<Task[]>([]);
 
-    addTasks(taskData: { title: string; description: string }) {
+    addTask(taskData: { title: string; description: string }) {
         const newTask: Task = {
             ...taskData,
             id: Math.random().toString(),
