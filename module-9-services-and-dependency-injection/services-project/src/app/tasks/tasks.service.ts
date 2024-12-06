69b13d9dff4aa8988cbe24f9e7d6570897838682
Siddharth Bhat <dbgt4531@gmail.com>
2024-12-06 16:53:15 +0530

    created custom injection token

--- module-9-services-and-dependency-injection/services-project/src/app/tasks/tasks.service.ts
+++ module-9-services-and-dependency-injection/services-project/src/app/tasks/tasks.service.ts
@@ -2,9 +2,9 @@ import { inject, Injectable, signal } from "@angular/core";
 import { Task, TaskStatus } from "./task.model";
 import { LoggingService } from "../logging.service";
 
-@Injectable({
+/* @Injectable({
     providedIn: "root"
-})
+}) */
 export class TasksService {
     private tasks = signal<Task[]>([]);
     private loggingService = inject(LoggingService);
