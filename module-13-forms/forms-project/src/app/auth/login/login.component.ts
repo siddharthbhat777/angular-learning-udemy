ab8eb09f47a631df3eb826552864ae872440c563
Siddharth Bhat <dbgt4531@gmail.com>
2024-12-08 21:35:25 +0530

    making form more efficient

--- module-13-forms/forms-project/src/app/auth/login/login.component.ts
+++ module-13-forms/forms-project/src/app/auth/login/login.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { afterNextRender, Component, DestroyRef, inject, viewChild } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
+import { debounceTime } from 'rxjs';
 
 @Component({
   selector: 'app-login',
@@ -9,6 +10,21 @@ import { FormsModule, NgForm } from '@angular/forms';
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  private form = viewChild.required<NgForm>('form');
+  private destroyRef = inject(DestroyRef);
+
+  constructor() {
+    afterNextRender(() => {
+      const subscription = this.form().valueChanges?.pipe(
+        debounceTime(500)
+      ).subscribe({
+        next: (value) => window.localStorage.setItem('saved-login-form', JSON.stringify({ email: value.email }))
+      });
+
+      this.destroyRef.onDestroy(() => subscription?.unsubscribe());
+    });
+  }
+
   onSubmit(formData: NgForm) {
     if (formData.form.invalid) {
       return;
@@ -16,5 +32,6 @@ export class LoginComponent {
     const enteredEmail = formData.form.value.email;
     const enteredPassword = formData.form.value.password;
     console.log(enteredEmail, enteredPassword);
+    formData.form.reset();
   }
 }
