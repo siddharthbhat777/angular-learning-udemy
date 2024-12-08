edb09f0ffbc594239bfa18728d5a6aa003a8e2b2
Siddharth Bhat <dbgt4531@gmail.com>
2024-12-08 15:47:53 +0530

    interceptors handling responses

--- module-12-http/http-project/frontend/src/main.ts
+++ module-12-http/http-project/frontend/src/main.ts
@@ -1,7 +1,8 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 
 import { AppComponent } from './app/app.component';
-import { HttpHandlerFn, HttpRequest, provideHttpClient, withInterceptors } from '@angular/common/http';
+import { HttpEventType, HttpHandlerFn, HttpRequest, provideHttpClient, withInterceptors } from '@angular/common/http';
+import { tap } from 'rxjs';
 
 function loggingInterceptor(request: HttpRequest<unknown>, next: HttpHandlerFn) {
     // Copying and modifying request data. Though below code crashes application.
@@ -9,7 +10,15 @@ function loggingInterceptor(request: HttpRequest<unknown>, next: HttpHandlerFn) 
         headers: request.headers.set('X-DEBUG', 'TESTING')
     }); */
     console.log('[Outgoing Request]', request);
-    return next(request);
+    return next(request).pipe(
+        tap({
+            next: event => {
+                if (event.type === HttpEventType.Response) {
+                    console.log('[Incoming Response]', event.status, event.body);
+                }
+            }
+        })
+    );
 }
 
 bootstrapApplication(AppComponent, {
