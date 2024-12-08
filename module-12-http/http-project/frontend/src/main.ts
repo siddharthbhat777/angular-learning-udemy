737d3a6efc938ee962ed1341b6d99a8b17b8d712
Siddharth Bhat <dbgt4531@gmail.com>
2024-12-08 15:41:49 +0530

    using interceptor to track http req/res

--- module-12-http/http-project/frontend/src/main.ts
+++ module-12-http/http-project/frontend/src/main.ts
@@ -1,9 +1,19 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 
 import { AppComponent } from './app/app.component';
-import { provideHttpClient } from '@angular/common/http';
+import { HttpHandlerFn, HttpRequest, provideHttpClient, withInterceptors } from '@angular/common/http';
 
+function loggingInterceptor(request: HttpRequest<unknown>, next: HttpHandlerFn) {
+    // Copying and modifying request data. Though below code crashes application.
+    /* const req = request.clone({
+        headers: request.headers.set('X-DEBUG', 'TESTING')
+    }); */
+    console.log('[Outgoing Request]', request);
+    return next(request);
+}
 
 bootstrapApplication(AppComponent, {
-    providers: [provideHttpClient()]
+    providers: [provideHttpClient(
+        withInterceptors([loggingInterceptor]) // used to watch overall app incoming and outgoing http request/response
+    )]
 }).catch((err) => console.error(err));
