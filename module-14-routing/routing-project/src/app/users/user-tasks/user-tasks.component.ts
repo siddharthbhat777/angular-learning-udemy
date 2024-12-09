4d74927eb87cd1e0c6475d4405b6a0f52b548d57
Siddharth Bhat <dbgt4531@gmail.com>
2024-12-09 23:45:28 +0530

    implementing nested routing

--- module-14-routing/routing-project/src/app/users/user-tasks/user-tasks.component.ts
+++ module-14-routing/routing-project/src/app/users/user-tasks/user-tasks.component.ts
@@ -1,28 +1,33 @@
-import { Component, computed, DestroyRef, inject, input, OnInit } from '@angular/core';
+import { Component, computed, inject, input } from '@angular/core';
 import { UsersService } from '../users.service';
-import { ActivatedRoute } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-user-tasks',
   standalone: true,
+  imports: [RouterOutlet],
   templateUrl: './user-tasks.component.html',
-  styleUrl: './user-tasks.component.css',
+  styleUrl: './user-tasks.component.css'
 })
-export class UserTasksComponent implements OnInit {
-  userName = '';
-  // userId = input.required<string>();
+export class UserTasksComponent {
+  // new way
+  userId = input.required<string>();
   private usersService = inject(UsersService);
-  private activatedRoute = inject(ActivatedRoute);
-  private destroyRef = inject(DestroyRef);
+  // old way
+  // userName = '';
+  // private activatedRoute = inject(ActivatedRoute);
+  // private destroyRef = inject(DestroyRef);
 
-  // userName = computed(() => this.usersService.users.find(u => u.id === this.userId())?.name);
+  // new way
+  userName = computed(() => this.usersService.users.find(u => u.id === this.userId())?.name);
 
-  ngOnInit(): void {
+  // old way
+  /* ngOnInit(): void {
       const subscription = this.activatedRoute.paramMap.subscribe({
         next: paramMap => {
           this.userName = this.usersService.users.find(u => u.id === paramMap.get('userId'))?.name || '';
         }
       });
       this.destroyRef.onDestroy(() => subscription.unsubscribe());
-  }
+  } */
 }
