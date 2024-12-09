2cf4d99cb8ca88fae1e6e5c7baf290d8f75a2408
Siddharth Bhat <dbgt4531@gmail.com>
2024-12-09 21:51:22 +0530

    adding password comparison validation

--- module-13-forms/forms-project/src/app/auth/signup/signup.component.ts
+++ module-13-forms/forms-project/src/app/auth/signup/signup.component.ts
@@ -1,5 +1,16 @@
 import { Component } from '@angular/core';
-import { FormArray, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AbstractControl, FormArray, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+
+function equalValues(controlName1: string, controlName2: string) {
+  return (control: AbstractControl) => {
+    const val1 = control.get(controlName1)?.value;
+    const val2 = control.get(controlName2)?.value;
+    if (val1 === val2) {
+      return null;
+    }
+    return { valuesNotEqual: true };
+  };
+}
 
 @Component({
   selector: 'app-signup',
@@ -20,6 +31,8 @@ export class SignupComponent {
       confirmPassword: new FormControl('', {
         validators: [Validators.required, Validators.minLength(6)]
       })
+    }, {
+      validators: [equalValues('password', 'confirmPassword')]
     }),
     userName: new FormGroup({
       firstName: new FormControl('', {
