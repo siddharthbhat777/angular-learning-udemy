00eddb69681cc7eb40259a13fbdf5c92943193fb
Siddharth Bhat <dbgt4531@gmail.com>
2024-12-10 18:08:25 +0530

    sorting actual data

--- module-14-routing/routing-project/src/app/tasks/tasks.component.ts
+++ module-14-routing/routing-project/src/app/tasks/tasks.component.ts
@@ -1,7 +1,6 @@
-import { Component, computed, DestroyRef, inject, input, OnInit } from '@angular/core';
+import { Component, computed, DestroyRef, inject, input, OnInit, signal } from '@angular/core';
 
 import { TaskComponent } from './task/task.component';
-import { Task } from './task/task.model';
 import { TasksService } from './tasks.service';
 import { ActivatedRoute, RouterLink } from '@angular/router';
 
@@ -15,20 +14,27 @@ import { ActivatedRoute, RouterLink } from '@angular/router';
 export class TasksComponent implements OnInit {
   userId = input.required<string>();
   private tasksService = inject(TasksService);
-  userTasks = computed(() => this.tasksService.allTasks().filter((task) => task.userId === this.userId()));
+  userTasks = computed(() => this.tasksService.allTasks().filter((task) => task.userId === this.userId()).sort((a, b) => {
+    if (this.order() === 'desc') {
+      return a.id > b.id ? -1 : 1;
+    } else {
+      return a.id > b.id ? 1 : -1;
+    }
+  }));
   
   // new way
   // order = input<'asc' | 'desc'>(); // input variable name should be same as query parameter key
 
   // old way
-  order?: 'asc' | 'desc';
+  // order?: 'asc' | 'desc';
+  order = signal<'asc' | 'desc'>('desc');
   private activatedRoute = inject(ActivatedRoute);
   private destroyRef = inject(DestroyRef);
 
   ngOnInit(): void {
     const subscription = this.activatedRoute.queryParams.subscribe({
       next: params => {
-        this.order = params['order'];
+        this.order.set(params['order']);
       }
     });
     this.destroyRef.onDestroy(() => subscription.unsubscribe());
