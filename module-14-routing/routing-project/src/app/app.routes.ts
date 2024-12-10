113f126dab49bce58eb41def0d1fc8a6ec45dea6
Siddharth Bhat <dbgt4531@gmail.com>
2024-12-10 19:45:26 +0530

    adding security to route

--- module-14-routing/routing-project/src/app/app.routes.ts
+++ module-14-routing/routing-project/src/app/app.routes.ts
@@ -1,8 +1,18 @@
-import { Routes } from "@angular/router";
+import { CanMatchFn, RedirectCommand, Router, Routes } from "@angular/router";
 import { routes as userRoutes } from "./users/users.routes";
 import { NoTaskComponent } from "./tasks/no-task/no-task.component";
 import { resolveTitle, resolveUserName, UserTasksComponent } from "./users/user-tasks/user-tasks.component";
 import { NotFoundComponent } from "./not-found/not-found.component";
+import { inject } from "@angular/core";
+
+const dummyCanMatch: CanMatchFn = (route, segments) => {
+    const router = inject(Router);
+    const shouldGetAccess = Math.random();
+    if (shouldGetAccess < 0.5) {
+        return true;
+    }
+    return new RedirectCommand(router.parseUrl('/unauthorized')); // used to redirect to some other route
+};
 
 export const routes: Routes = [
     {
@@ -14,6 +24,7 @@ export const routes: Routes = [
         path: 'users/:userId',
         component: UserTasksComponent,
         children: userRoutes,
+        canMatch: [dummyCanMatch],
         data: { // to pass static data
             message: 'Hello!'
         },
