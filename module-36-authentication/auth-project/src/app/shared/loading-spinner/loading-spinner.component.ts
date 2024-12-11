62e4279506730431101177013ebbc287532212f9
Siddharth Bhat <dbgt4531@gmail.com>
2024-12-11 22:28:48 +0530

    added signup authentication and loading spinner

---
+++ module-36-authentication/auth-project/src/app/shared/loading-spinner/loading-spinner.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-loading-spinner',
+    standalone: false,
+    template: '<div class="loadingio-spinner-dual-ring-nq4q5u6dq7r"><div class="ldio-x2uulkbinbj"><div></div><div><div></div></div></div></div>',
+    styleUrls: ['./loading-spinner.component.css']
+})
+export class LoadingSpinnerComponent {}
