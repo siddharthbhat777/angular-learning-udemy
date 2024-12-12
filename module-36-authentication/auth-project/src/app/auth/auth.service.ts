7b6b9aa75c9921045d5de406eb82150c2ca3c08a
Siddharth Bhat <dbgt4531@gmail.com>
2024-12-12 15:57:56 +0530

    logout now working

--- module-36-authentication/auth-project/src/app/auth/auth.service.ts
+++ module-36-authentication/auth-project/src/app/auth/auth.service.ts
@@ -2,6 +2,7 @@ import { HttpClient, HttpErrorResponse } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { BehaviorSubject, catchError, tap, throwError } from "rxjs";
 import { User } from "./user.model";
+import { Router } from "@angular/router";
 
 export interface AuthResponseData {
     idToken: string;
@@ -18,7 +19,7 @@ export interface AuthResponseData {
 export class AuthService {
     user = new BehaviorSubject<User>(null);
 
-    constructor(private http: HttpClient) { }
+    constructor(private http: HttpClient, private router: Router) { }
 
     signup(email: string, password: string) {
         return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAQPEEKesuoYN8eEprGt3V8Ajw4gYV55Po', {
@@ -38,6 +39,11 @@ export class AuthService {
         }).pipe(catchError(this.handleError), tap(resData => {
             this.handleAuthentication(resData.email, resData.localId, resData.idToken, +resData.expiresIn);
         }));
+    }
+
+    logout() {
+        this.user.next(null);
+        this.router.navigate(['/auth']);
     }
 
     private handleAuthentication(email: string, userId: string, token: string, expiresIn: number) {
