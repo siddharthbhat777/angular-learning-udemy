4c8db901ef06a43813e84e2f716e4e1049ef22c2
Siddharth Bhat <dbgt4531@gmail.com>
2024-12-12 17:04:19 +0530

    auto login feature added

--- module-36-authentication/auth-project/src/app/app.component.ts
+++ module-36-authentication/auth-project/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from './auth/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -6,10 +7,9 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  loadedFeature = 'recipe';
-
-  onNavigate(feature: string) {
-    this.loadedFeature = feature;
+export class AppComponent implements OnInit {
+  constructor(private authService: AuthService) { }
+  ngOnInit(): void {
+    this.authService.autoLogin();
   }
 }
