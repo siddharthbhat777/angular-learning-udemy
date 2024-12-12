6241f0d7ca2b31f263b499edfc04bcc0e5f62720
Siddharth Bhat <dbgt4531@gmail.com>
2024-12-12 17:36:07 +0530

    using route guard to restrict access to unauthorized routes

---
+++ module-36-authentication/auth-project/src/app/auth/auth.guard.ts
@@ -0,0 +1,23 @@
+import { Injectable } from "@angular/core";
+import { ActivatedRouteSnapshot, CanActivate, GuardResult, MaybeAsync, Router, RouterStateSnapshot } from "@angular/router";
+import { AuthService } from "./auth.service";
+import { map, take } from "rxjs";
+
+@Injectable({
+    providedIn: "root"
+})
+export class AuthGuardService implements CanActivate {
+    constructor(private authService: AuthService, private router: Router) {}
+
+    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): MaybeAsync<GuardResult> {
+        return this.authService.user.pipe(
+            take(1),
+            map(user => {
+            const isAuth = !!user;
+            if (isAuth) {
+                return true;
+            }
+            return this.router.createUrlTree(['/auth']);
+        }));
+    }
+}
