4400b65efddb68257ed6dcac7bb4d922cde9808e
Siddharth Bhat <dbgt4531@gmail.com>
2025-01-25 21:10:06 +0530

    modify data with actions

--- module-41-ngrx/ngrx-project/src/app/counter-controls/counter-controls.component.ts
+++ module-41-ngrx/ngrx-project/src/app/counter-controls/counter-controls.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-
-import { CounterService } from '../counter.service';
+import { Store } from '@ngrx/store';
+import { increment } from '../store/counter.actions';
 
 @Component({
   selector: 'app-counter-controls',
@@ -9,13 +9,12 @@ import { CounterService } from '../counter.service';
   standalone: true,
 })
 export class CounterControlsComponent {
-  constructor(private counterService: CounterService) {}
+  constructor(private store: Store) {}
 
   increment() {
-    this.counterService.increment();
+    this.store.dispatch(increment());
   }
 
   decrement() {
-    this.counterService.decrement();
   }
 }
