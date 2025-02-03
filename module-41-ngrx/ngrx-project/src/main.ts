9840eecf386fa2afa7c7c4724cf5fa92091e6c35
Siddharth Bhat <dbgt4531@gmail.com>
2025-02-03 21:47:32 +0530

    setting up ngrx effects

--- module-41-ngrx/ngrx-project/src/main.ts
+++ module-41-ngrx/ngrx-project/src/main.ts
@@ -3,10 +3,11 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { provideStore } from '@ngrx/store';
 import { counterReducer } from './app/store/counter.reducer';
+import { provideEffects } from '@ngrx/effects';
 
 bootstrapApplication(AppComponent, {
     providers: [provideStore({
         // auth: authreducer, // can add multiple reducers
         counter: counterReducer
-    })]
+    }), provideEffects()]
 });
