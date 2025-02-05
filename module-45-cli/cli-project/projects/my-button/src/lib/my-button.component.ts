4fbd823676b345a8fff265e601ae86fd9bae4311
Siddharth Bhat <dbgt4531@gmail.com>
2025-02-05 18:42:55 +0530

    created library

---
+++ module-45-cli/cli-project/projects/my-button/src/lib/my-button.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-my-button',
+  template: `
+    <p>
+      my-button works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class MyButtonComponent {
+
+}
