d72d4a6bcd21318c6ffe05a4be119dea744f7c67
Siddharth Bhat <dbgt4531@gmail.com>
2025-02-05 22:48:55 +0530

    added standalone and signals module

---
+++ module-40-signals/signals-project/src/app/signals/signals.component.ts
@@ -0,0 +1,29 @@
+import { NgFor } from '@angular/common';
+import { Component, signal, computed, effect } from '@angular/core';
+
+@Component({
+  selector: 'app-signals',
+  templateUrl: './signals.component.html',
+  standalone: true,
+  imports: [NgFor],
+})
+export class SignalsComponent {
+  actions = signal<string[]>([]);
+  counter = signal(0);
+  doubleCounter = computed(() => this.counter() * 2);
+
+  constructor() {
+    effect(() => console.log(this.counter()));
+  }
+
+  increment() {
+    // this.counter.update((oldCounter) => oldCounter + 1);
+    this.counter.set(this.counter() + 1);
+    this.actions.mutate((oldActions) => oldActions.push('INCREMENT'));
+  }
+
+  decrement() {
+    this.counter.update((oldCounter) => oldCounter - 1);
+    this.actions.update((oldActions) => [...oldActions, 'DECREMENT']);
+  }
+}
